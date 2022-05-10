f27100755285c982e7ff2297b234a671324677ce
stefanom13 <stefanomancuso13@gmail.com>
2022-05-10 21:24:27 +0200

    ottimizzazione chiamata api.php

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts','Api\PostController@index');
+Route::namespace('api')->group(function (){
+    Route::get('/posts','PostController@index');
+});
